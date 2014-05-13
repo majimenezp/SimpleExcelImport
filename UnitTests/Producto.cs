0d5c3f8677045ee2667acab71cfb8291266a68a8
majimenezp <majimenezp@taan.com.mx>
2014-05-13 12:08:42 -0500

    Version inicial

---
+++ UnitTests/Producto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleExcelImport;
+namespace UnitTests
+{
+    public class Producto
+    {
+        [ExcelImport("Codigo",order=1)]
+        public string Codigo { get; set; }
+
+        [ExcelImport("Descripcion", order = 2)]
+        public string Descripcion { get; set; }
+
+        [ExcelImport("Seccion", order = 3)]
+        public string Seccion { get; set; }
+
+        [ExcelImport("Categoria", order = 4)]
+        public string Categoria { get; set; }
+
+        [ExcelImport("IsLAN", order = 5)]
+        public string IsLAN { get; set; }
+
+        [ExcelImport("UMPri", order = 6)]
+        public string UMPri { get; set; }
+
+        [ExcelImport("UMSec", order = 7)]
+        public string UMSec { get; set; }
+
+        [ExcelImport("UMRec", order = 8)]
+        public string UMRec { get; set; }
+
+        [ExcelImport("FactorSec", order = 9)]
+        public string FactorSec { get; set; }
+
+        [ExcelImport("FactorRec", order = 10)]
+        public string FactorRec { get; set; }
+        
+        [ExcelImport("ClaseContable", order = 11)]
+        public string ClaseContable { get; set; }
+
+        public Producto()
+        {
+
+        }
+    }
+}
---
+++ UnitTests/Read tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using SimpleExcelImport;
+using System.Collections.Generic;
+using System.Reflection;
+namespace UnitTests
+{
+    [TestClass]
+    public class ReadTest
+    {
+        [TestMethod]
+        public void ReadFile()
+        {
+            string path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            var data=File.ReadAllBytes(Path.Combine(path,"productos.xls"));
+            ImportFromExcel import = new ImportFromExcel();
+            import.LoadXls(data);
+            List<Producto> output=import.ExcelToList<Producto>(0,1);
+            Assert.IsTrue(output.Count > 0);
+
+        }
+    }
+}
